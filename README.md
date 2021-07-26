a39e0b8e524a87f3e92418e035de0ca011f9b912
Ahmed Khairy <akhairy.c@channels.com.sa>
2021-07-26 17:37:44 +0200

    develop gateways APIs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# gateways-api
+# Gateways
+Gatwaways is a Task for Musala Soft
+
+## Gateways - Angular Project
+
+You can navigate to `https://github.com/ahmedkhairydev/gateways` to get the project
+
+## Development server
+
+Run `npm install` then `npm run start` for a dev server. Navigate to `http://localhost:300/`.
---
+++ server.js
@@ -0,0 +1,76 @@
+// Setup empty JS object to act as endpoint for all routes
+let gateways = []
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+// Dependencies
+const bodyParser = require('body-parser');
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('gateways'));
+
+// Setup Server
+const port = 3000;
+
+const listening = () => {
+    console.log('Hey, server running...');
+};
+
+// start the server
+app.listen(port, listening);
+
+// module name
+const moduleName = 'Gateway';
+
+// Get All Gateways API
+app.get(`/${moduleName}/GetAll`, (request, response) => {
+    response.send({
+        status: 200,
+        body: gateways
+    });
+});
+
+// add Gateway API
+app.post(`/${moduleName}/Add`, (request, response) => {
+    request.body.id = Date.now().toString();
+    gateways.push(request.body);
+    console.log(gateways);
+    response.send({ status: 200 });
+});
+
+// add Gateway API
+app.post(`/${moduleName}/Update/:id`, (request, response) => {
+    const gatewayIndex = gateways.indexOf(gateways.find(gateway => gateway.id === request.params.id));
+    gateways[gatewayIndex] = request.body;
+    response.send({ status: 200 });
+});
+
+// Get Specific Gateway API
+app.get(`/${moduleName}/Get/:id`, (request, response) => {
+    if (gateways.find(gateway => gateway.id === request.params.id)) {
+        response.send({
+            status: 200,
+            body: gateways.find(gateway => gateway.id === request.params.id)
+        });
+    } else {
+        response.status(500).send({ status: 500, message: "There's no gateway with this Id" });
+    }
+});
+
+// 404 API handling
+app.use((req, res, next) => {
+    res.status(404).send({ status: 404, message: 'Not Found' });
+});
